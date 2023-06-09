49511f94e816346bf03e4079fb18fa0f230e8eac
Larynt Sawfa Kenanga <109521914+lsawfa@users.noreply.github.com>
2023-06-09 07:54:17 +0700

    latihan

--- Latihan/Java/BFS&DFS_Traversal/src/DFSTraversal.java
+++ Latihan/Java/BFS&DFS_Traversal/src/DFSTraversal.java
@@ -1,14 +1,16 @@
 import java.util.*;
-public class DFSTraversal {
+
+class DFSTraversal {
     private LinkedList<Integer> adj[];
     private boolean visited[];
 
-    DFSTraversal(int v) {
-        adj = new LinkedList[v];
-        visited = new boolean[v];
-        for (int i = 0; i < v; i++) {
+    DFSTraversal(int V)
+    {
+        adj = new LinkedList[V];
+        visited = new boolean[V];
+
+        for (int i = 0; i < V; i++)
             adj[i] = new LinkedList<Integer>();
-        }
     }
 
     void insertEdge(int src, int dest) {
@@ -18,17 +20,18 @@ public class DFSTraversal {
     void DFS(int vertex) {
         visited[vertex] = true;
         System.out.print(vertex + " ");
+
         Iterator<Integer> it = adj[vertex].listIterator();
         while (it.hasNext()) {
             int n = it.next();
-            if (!visited[n]) {
+            if (!visited[n])
                 DFS(n);
-            }
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         DFSTraversal graph = new DFSTraversal(8);
+
         graph.insertEdge(0, 1);
         graph.insertEdge(0, 2);
         graph.insertEdge(0, 3);
@@ -38,8 +41,9 @@ public class DFSTraversal {
         graph.insertEdge(3, 6);
         graph.insertEdge(4, 7);
         graph.insertEdge(4, 5);
-        graph.insertEdge(2, 2);
-        System.out.println("Depth First Traversal for the graph is: ");
+        graph.insertEdge(5, 2);
+
+        System.out.println("Depth First Traversal for the graph is:");
         graph.DFS(0);
     }
 }
